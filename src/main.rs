458322aa098df7f9d9f3242f18937094ef6ba832
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-07-09 13:54:40 +0100

    Remove yew to test Leptos

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,3 @@
-use yew::prelude::*;
-
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
-    html! {
-        <div>
-            <h1 class="title">{ "Hello" }</h1>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
-    }
-}
-
 fn main() {
-    yew::Renderer::<App>::new().render();
+    println!("Hello, world!");
 }
